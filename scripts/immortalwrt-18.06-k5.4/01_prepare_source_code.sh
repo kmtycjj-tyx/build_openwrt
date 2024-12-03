0e4ff6004b6e515b0df82b7db987eb66036545e0
kmtycjj-tyx <67580609+kmtycjj-tyx@users.noreply.github.com>
2024-12-03 14:46:55 +0800

    Update 01_prepare_source_code.sh

--- scripts/immortalwrt-18.06-k5.4/01_prepare_source_code.sh
+++ scripts/immortalwrt-18.06-k5.4/01_prepare_source_code.sh
@@ -12,6 +12,6 @@ clone_repo $openclash_repo master openclash &
 wait
 
 # 修改默认 IP 为 192.168.1.99
-sed -i 's/192.168.1.1/192.168.1.99/g' openwrt/package/base-files/files/bin/config_generate
+sed -i 's/192.168.1.1/192.168.2.254/g' openwrt/package/base-files/files/bin/config_generate
 
 exit 0
